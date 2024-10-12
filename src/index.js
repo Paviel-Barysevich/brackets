4b2a19f7e1e65394b9adfea31c6c15d90489e2c5
Paviel Barysevich-Bleyaniuk <pavelmessage@gmail.com>
2024-10-12 12:35:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) return false;
+
+  let bracketsObj = {};
+  let arr = [];
+
+  bracketsConfig.map((bracketsPair) => bracketsObj[bracketsPair[0]] = bracketsPair[1]);
+
+  for (let i = 0; i < str.length; i++) {
+    if (arr.length === 0) {
+      arr.push(str[i]);
+      continue;
+    }
+
+    let bracketLeft = arr.at(-1);
+
+    if (bracketsObj[bracketLeft] === str[i]) {
+      arr.pop();
+    } else {
+      arr.push(str[i]);
+    }
+  }
+
+  return (arr.length === 0);
 }
